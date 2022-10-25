9ddc00636367231b095ef79cb1a7d6a40b082c04
Muchnik Illia <muchnik.illia.js@gmail.com>
2022-10-25 10:34:50 +0300

    Pokemon Card in progress

--- src/componens/PokemonCard.jsx
+++ src/componens/PokemonCard.jsx
@@ -9,7 +9,7 @@ export default function PokemonCard({ pokemon }) {
 
   const { name, height, base_experience, weight, pokemon_v2_pokemonspecy, pokemon_v2_pokemontypes  } = pokemon
   const  specyName= pokemon_v2_pokemonspecy.name
-
+  const types = pokemon_v2_pokemontypes.map(t=>t.pokemon_v2_type.name).join(', ')
   return (
     <StyledCard>
       <CardContent>
@@ -18,6 +18,7 @@ export default function PokemonCard({ pokemon }) {
         <Typography variant="h6">experience: {base_experience}</Typography>
         <Typography variant="h6">weight: {weight}</Typography>
         <Typography variant="h6">specy: {specyName}</Typography>
+        <Typography variant="h6">types: {types}</Typography>
       </CardContent>
     </StyledCard>
   )
@@ -25,6 +26,6 @@ export default function PokemonCard({ pokemon }) {
 
 const StyledCard = styled(Card)(() => ({
   background: '#eeeeee',
-  width: '47%',
+  width: '30%',
   marginTop: '20px',
 }))
